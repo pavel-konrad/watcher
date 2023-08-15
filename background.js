f0df1cc22cc1ccd3042588eab9835b954ec0d8a1
Pavel Konrad <pavelkonrad@icloud.com>
2023-08-15 14:05:19 +0200

    obnova background.js

--- background.js
+++ background.js
@@ -1,46 +1,56 @@
-// Získání aktuální IP adresy uživatele pomocí externí služby
-fetch('https://api.ipify.org?format=json')
-  .then(response => response.json())
-  .then(data => {
-    const userIpAddress = data.ip;
-    // Volání funkce pro uložení IP adresy do chrome.storage
-    saveIpAddressToStorage(userIpAddress);
-  })
-  .catch(error => console.error('Chyba při získávání IP adresy:', error));
-
-// Funkce pro uložení IP adresy do chrome.storage
-function saveIpAddressToStorage(ipAddress) {
-  chrome.storage.local.set({ userIpAddress: ipAddress }, function() {
-    console.log('IP adresa uložena:', ipAddress);
-  });
-}
-
-// Poslouchání zpráv z content skriptu
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.type === 'savePageVisitData') {
-    // Zpracování zprávy a uložení dat o stránce
-    const url = request.url;
-    const timeSpent = request.timeSpent;
-    savePageVisitData(url, timeSpent);
-    sendResponse({ message: 'Data byla uložena.' });
-  }
-});
-
-// Funkce pro uložení dat o stránce do chrome.storage
-function savePageVisitData(url, timeSpent) {
-    console.log('Ukládání dat o návštěvě stránky...');
+// Save data to chrome.storage
+function savePageVisitData(ipAddress, url, timeSpent, pageTitle) {
   chrome.storage.local.get({ pageVisits: [] }, function(data) {
     const pageVisits = data.pageVisits;
 
-    // Přidání nového záznamu o stránce do dat
+    // Adding a new page record to the data
     pageVisits.push({
+      ipAddress: ipAddress,
       url: url,
-      timeSpent: timeSpent
+      timeSpent: timeSpent,
+      pageTitle: pageTitle,
     });
 
-    // Aktualizace dat v lokálním úložišti
+    // Updating data in local storage
     chrome.storage.local.set({ pageVisits: pageVisits }, function() {
       console.log('Data o návštěvě stránky uložena:', pageVisits);
     });
   });
+}
+
+// Listening messages from storage
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.type === 'savePageVisitData') {
+    // Massage processing
+    const url = request.url;
+    const timeSpent = request.timeSpent;
+    const pageTitle = request.pageTitle;
+
+    // Get local IP address
+    getLocalIpAddress().then((ipAddress) => {
+      savePageVisitData(ipAddress, url, timeSpent, pageTitle);
+      sendResponse({ message: 'Data byla uložena.' });
+    });
+  }
+
+  // Calling sendResponse
+  return true;
+});
+
+// Get local IP address
+async function getLocalIpAddress() {
+  try {
+    const peerConnection = new RTCPeerConnection({ iceServers: [] });
+    const sdpOffer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(sdpOffer);
+    
+    const localIp = peerConnection.localDescription.sdp.match(/(\d+\.\d+\.\d+\.\d+)/)[0];
+    
+    peerConnection.close();
+    
+    return localIp;
+  } catch (error) {
+    console.error('Chyba při získávání lokální IP adresy:', error);
+    return null;
+  }
 }
